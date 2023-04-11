af9c9b655d9831014394ffc6c2421df21549ff7c
lianajayde <lianajaydee@gmail.com>
2023-04-11 11:10:09 -0500

    updated seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,10 @@
 USE employees_db;
 
 INSERT INTO department (name)
-VALUES
-    ("Sales"),
-    ("Back End"),
-    ("Research"),
-    ("Finance");
+VALUES ("Sales"),
+       ("Back End"),
+       ("Research"),
+       ("Finance");
 
 INSERT INTO role (title, salary, department_id)
 VALUES ("Sales Representative", 40000, 1),
@@ -19,7 +18,7 @@ INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("April", "Marin", 4, NULL),
        ("Alvin", "Carl", 5, NULL),
        ("Jameson", "Daniels", 6, NULL),
-       ("Jared", "Padalecki", 10, NULL),
+       ("Jared", "Padalecki", 3, NULL),
        ("Merissa", "Garcia", 8, NULL),
        ("Jensen", "Ackles", 1, 4),
        ("Amber", "Bent", 1, 1),
