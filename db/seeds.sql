e6e960c10cb39a7da0f07fff10dd7be79b3baef9
lianajayde <lianajaydee@gmail.com>
2023-04-11 11:02:09 -0500

    changes

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,22 +8,21 @@ VALUES
     ("Finance");
 
 INSERT INTO role (title, salary, department_id)
-VALUES
-    ("Sales Representative", 40000, 1),
-    ("Computer Engineer", 90000, 2),
-    ("Researcher", 30000, 3),
-    ("Accountant", 80000, 4);
+VALUES ("Sales Representative", 40000, 1),
+       ("Computer Engineer", 90000, 2),
+       ("Researcher", 30000, 3),
+       ("Accountant", 80000, 4),
+       ("Account Manager", 144000, 6);
     
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES
-    ("April", "Marin", 4, null),
-    ("Alvin", "Carl", 1, 1),
-    ("Jameson", "Daniels", 1, 1),
-    ("Jensen", "Ackles", 1, 1),
-    ("Jared", "Padalecki", 2, null),
-    ("Merissa", "Garcia", 2, null),
-    ("Amber", "Bent", 1, 1),
-    ("Brady", "Arneg", 2, 1);
-    ("Zane", "Junior", 1, 1),
-    ("Kelly", "Smith", 2, 1);
+VALUES ("April", "Marin", 4, NULL),
+       ("Alvin", "Carl", 5, NULL),
+       ("Jameson", "Daniels", 6, NULL),
+       ("Jared", "Padalecki", 10, NULL),
+       ("Merissa", "Garcia", 8, NULL),
+       ("Jensen", "Ackles", 1, 4),
+       ("Amber", "Bent", 1, 1),
+       ("Brady", "Arneg", 2, 1),
+       ("Zane", "Jak", 3, 4),
+       ("Kelly", "Smith", 1, 4);
